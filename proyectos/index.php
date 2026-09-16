e12f24839d41f19f626296a04a5e375cbcd41ec8
X-Jeff-12 <vg24028@ues.edu.sv>
2026-09-16 13:11:30 -0600

    feat: inicio del formulario html

--- proyectos/index.php
+++ proyectos/index.php
@@ -2,6 +2,7 @@
 
 $session_start();
 
+
 if(!isset($_SESSION["pedidos"])){
     $_SESSION["pedidos"] = [];
 }
@@ -10,29 +11,96 @@ $productos = [
     "cafe" =>[
         "nombre" => "Cafe Caliente",
         "categoria" => "bebidas",
-        "precio" => 1.50,
+        "precio" => 5.50,
         "disponibles" => 30
     ],
     "semitas" =>[
         "nombre" => "Semitas",
         "categoria" => "pan dulce",
-        "precio" => 0.50,
+        "precio" => 3.50,
         "disponibles" => 40
     ],
     "pastel" =>[
         "nombre" => "Pastel mani",
         "categoria" => "Pasteles",
-        "precio" => 2.50,
+        "precio" => 10.50,
         "disponibles" => 25
     ],
     "chocolota" =>[
         "nombre" => "Chocolate Caliente",
         "categoria" => "bebidas",
-        "precio" => 1.50,
+        "precio" => 5.50,
+        "disponibles" => 30
+    ],
+    "desayuno" =>[
+        "nombre" => "Huevos con casamiento",
+        "categoria" => "alimentos",
+        "precio" => 15.50,
         "disponibles" => 30
     ],
 ];
 
+function calcularPago(float $total){
 
+}
+$errores = [];
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+    $nombre = trim($_POST["nombre"]) ?? [];
+    $cantidad = trim($_POST["cantidad"]) ?? [];
+    $productos = trim($_POST["producto"]) ?? [];
+    
+    if(empty($nombre) || empty($cantidad) ){
+        $errores[] ="Facil relleno todos los campos obligatorios"; 
+    }
+
+
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Seleccione sus alimentos</h2>
+
+    <?php if(!empty($errores)):?>
+        <div>
+            <ul>
+                <?php foreach($errores as $error): ?>
+                    <li> <?= htmlspecialchars($error)?> </li>    
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+
+    <div>
+        <form action="" METHOD = "POST">
+            <LABEl>Nombre</LABEl><br>
+            <input type="text" name="nombre"><br>
+            
+            <LABEl>Nombre</LABEl><br>
+            <input type="text" name="nombre"><br>
+            
+            <LABEl>Nombre</LABEl><br>
+            <input type="text" name="nombre"><br>
+            
+            <LABEl>Nombre</LABEl><br>
+            <input type="text" name="nombre"><br>
+            
+            <LABEl>Nombre</LABEl><br>
+            <input type="text" name="nombre"><br>
+            
+            
+
+
+
+
+        </form>
+    </div>
+</body>
+</html>
